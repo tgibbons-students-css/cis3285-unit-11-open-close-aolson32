9da5778e8bf1518a84d7a401dded69deddf59397
Anna Olson <aolson32@css.edu>
2022-11-17 10:34:03 -0600

    Learning Activity Part 1

--- AbstractTrader/Program.cs
+++ AbstractTrader/Program.cs
@@ -13,8 +13,8 @@ namespace AbstractTrader
         {
             var tradeStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("VirtualTrader.trades4.txt");
 
-            //TradeProcessor tradeProcessor = new TradeProcessorVersion1();
-            TradeProcessor tradeProcessor = new TradeProcessorVersion2();
+            //ITradeProcesser tradeProcessor = new TradeProcessorVersion1();
+            ITradeProcesser tradeProcessor = new TradeProcessorVersion2();
 
             // ============= do not change anything below this line =============
             tradeProcessor.ProcessTrades(tradeStream);
